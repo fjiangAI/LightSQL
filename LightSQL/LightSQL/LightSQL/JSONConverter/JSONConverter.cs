4016b86008db0355ce4e2cd62cb3fbb18f99f3c6
liuxuan320 <jf940927320@163.com>
2016-09-03 18:31:20 +0800

    second commit add JSONConverter and XMLConverter

---
+++ LightSQL/LightSQL/LightSQL/JSONConverter/JSONConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace LightSQL.JSONConverter
+{
+    /// <summary>
+    /// 只有序列化和反序列化操作的JSON类
+    /// </summary>
+   public static class JSONConverter
+    {
+        /// <summary>
+        /// 这是序列化一个List
+        /// </summary>
+        /// <typeparam name="T">这是实体类</typeparam>
+        /// <param name="old">这是实体列表</param>
+        /// <returns>返回的是JSON格式字符串</returns>
+        public static string GetJsonString<T>(List<T> old)
+        {
+            return new JavaScriptSerializer().Serialize(old);
+        }
+        /// <summary>
+        /// 这是反序列化，也就是JSON传递给List的
+        /// </summary>
+        /// <typeparam name="T">要变成的实体类型</typeparam>
+        /// <param name="JsonStr">原Json格式字符串</param>
+        /// <returns>List列表</returns>
+        public static List<T> JSONStringToList<T>(string JsonStr)
+        {
+            List<T> objs =new JavaScriptSerializer().Deserialize<List<T>>(JsonStr);
+            return objs;
+        }
+    }
+}
---
+++ LightSQL/LightSQL/LightSQL/XMLConverter/XMLConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LightSQL.XMLConverter
+{
+    /// <summary>
+    /// 只有序列化和反序列化操作的XML类
+    /// </summary>
+    public static class XMLConverter
+    {
+        /// <summary>
+        /// 根据对象，来序列化这个对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="entity">对象类</param>
+        /// <returns></returns>
+        public static string XMLSerialize<T>(T entity)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextWriter writer = new StringWriter(buffer))
+            {
+                serializer.Serialize(writer, entity);
+            }
+
+            return buffer.ToString();
+
+        }
+        /// <summary>
+        /// 这是反序列化，根据一个字符串，返回一个对象。
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="xmlString">XML字符串</param>
+        /// <returns>目标对象</returns>
+        public static T DeXMLSerialize<T>(string xmlString)
+        {
+            T cloneObject = default(T);
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append(xmlString);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextReader reader = new StringReader(buffer.ToString()))
+            {
+                Object obj = serializer.Deserialize(reader);
+                cloneObject = (T)obj;
+            }
+            return cloneObject;
+        }
+    }
+}
